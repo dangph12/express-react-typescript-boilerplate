eb347f0d2c99e74b5cdd10ce00ad05d45fcae1c8
dangph12 <dangphhe181217@fpt.edu.vn>
2025-05-20 15:42:40 +0700

    chore: remove static dirname

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
@@ -18,7 +17,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
   cors({

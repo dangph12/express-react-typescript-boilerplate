ab0af51645c06f25e9ccc85ff7f851e50ae4538d
dangph12 <dangphhe181217@fpt.edu.vn>
2025-06-17 17:29:02 +0700

    feat: use authorize middleware

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,16 +1,15 @@
 import { Router } from 'express';
-import passport from 'passport';
-import quizRoutes from '~/modules/quiz/quizRoute';
 import authRoutes from '~/modules/auth/authRoute';
+import userRoute from '~/modules/user/userRoute';
+import { authorize } from '~/middleware/authorize';
+import jwtAuth from '~/middleware/jwtAuth';
 
 const router = Router();
 // Non-auth routes
 router.use('/auth', authRoutes);
+
 // Auth routes
-router.use(
-  '/quizzes',
-  passport.authenticate('jwt', { session: false }),
-  quizRoutes
-);
+
+router.use('/users', jwtAuth, authorize('admin'), userRoute);
 
 export default router;
